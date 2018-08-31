efbf5724614ccb2a93ae3b7f5202747501576cde
Chenjie Ni <chenjie.ni@mail.utoronto.ca>
2018-08-31 19:02:59 -0400

    Upload files.

---
+++ main.py
@@ -0,0 +1,154 @@
+# Numpy
+import numpy as np
+
+# Torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+# Torchvision
+import torchvision
+import torchvision.transforms as transforms
+
+# Matplotlib
+import matplotlib.pyplot as plt
+
+# OS
+import os
+import argparse
+
+# Set random seed for reproducibility
+SEED = 87
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(SEED)
+
+
+def print_model(encoder, decoder):
+    print("============== Encoder ==============")
+    print(encoder)
+    print("============== Decoder ==============")
+    print(decoder)
+    print()
+
+
+def create_model():
+    autoencoder = Autoencoder()
+    print_model(autoencoder.encoder, autoencoder.decoder)
+    if torch.cuda.is_available():
+        autoencoder = autoencoder.cuda()
+        print("Model moved to GPU in order to speed up training.")
+    return autoencoder
+
+
+def get_torch_vars(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        # Input size: [batch, 3, 32, 32]
+        # Output size: [batch, 3, 32, 32]
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 6, 3, stride=1, padding=1),            # [batch, 6, 32, 32]
+            nn.MaxPool2d(2),                                    # [batch, 6, 16, 16]
+            nn.ReLU(),
+            nn.Conv2d(6, 12, 3, stride=1, padding=1),           # [batch, 12, 16, 16]
+            nn.MaxPool2d(2),                                    # [batch, 12, 8, 8]
+            nn.ReLU(),
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(12, 6, 3, stride=1, padding=1),  # [batch, 6, 8, 8]
+            nn.Upsample(scale_factor=2),                        # [batch, 6, 16, 16]
+            nn.ReLU(),
+            nn.ConvTranspose2d(6, 3, 3, stride=1, padding=1),   # [batch, 3, 16, 16]
+            nn.Upsample(scale_factor=2),                        # [batch, 3, 32, 32]
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return encoded, decoded
+
+def main():
+    parser = argparse.ArgumentParser(description="Train Autoencoder")
+    parser.add_argument("--valid", action="store_true", default=False,
+                        help="Perform validation only.")
+    args = parser.parse_args()
+
+    # Create model
+    autoencoder = create_model()
+
+    # Load data
+    transform = transforms.Compose(
+        [transforms.ToTensor(), ])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=16,
+                                              shuffle=True, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=16,
+                                             shuffle=False, num_workers=2)
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    if args.valid:
+        print("Loading checkpoint...")
+        autoencoder.load_state_dict(torch.load("./weights/autoencoder.pkl"))
+        dataiter = iter(testloader)
+        images, labels = dataiter.next()
+        print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(16)))
+        imshow(torchvision.utils.make_grid(images))
+
+        encoded_imgs, decoded_imgs = autoencoder(images)
+        imshow(torchvision.utils.make_grid(decoded_imgs.data))
+
+        exit(0)
+
+    # Define an optimizer and criterion
+    criterion = nn.MSELoss()
+    optimizer = optim.Adadelta(autoencoder.parameters())
+
+    for epoch in range(5):
+        running_loss = 0.0
+        for i, (inputs, _) in enumerate(trainloader, 0):
+            inputs = get_torch_vars(inputs)
+
+            # ============ Forward ============
+            encoded, outputs = autoencoder(inputs)
+            loss = criterion(outputs, inputs)
+            # ============ Backward ============
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # ============ Logging ============
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+    print('Saving Model...')
+    if not os.path.exists('./weights'):
+        os.mkdir('./weights')
+    torch.save(autoencoder.state_dict(), "./weights/autoencoder.pkl")
+
+
+if __name__ == '__main__':
+    main()
